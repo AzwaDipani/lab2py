a754e475236fab0200c6605789977090885a140c
AzwaDipani <destadwiputra3@gmail.com>
2022-10-26 22:25:55 +0700

    perubahan

---
+++ latihan/latihan3.py
@@ -0,0 +1,19 @@
+# input nilai variable
+a=input("masukan nilai a:")
+b=input("masukan nilai b:")
+
+# cetak nilai variable
+print("variable a=",a)
+print("variable b=",b)
+
+# cetak hasil operasi kedua variable dengan string format
+a=int(a)
+b=int(b)
+print("hasil perkalian {1}*{0}=%d".format(a,b) % (a*b))
+
+# konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d".format (a,b) % (a+b))
+print("hasil pembagian {1}/{0}=%d".format (a,b) % (a/b))
+
